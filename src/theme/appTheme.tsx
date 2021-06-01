b6778815f6e3534cbc0311a8b05a0e06e81ddc98
Gilbert Marin <daviddlioncourt@gmail.com>
2021-06-01 07:30:14 -0600

    My first calculator on React Native

---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable prettier/prettier */
+import { StyleSheet } from "react-native";
+
+
+
+export const styles = StyleSheet.create({
+    fondo: {
+        flex: 1,
+        backgroundColor: 'black',
+        
+
+    }, 
+    calculadoraContainer: {
+        flex: 1,
+        paddingHorizontal: 20,
+        justifyContent:'flex-end',
+    },
+    resultado: {
+        color: 'white',
+        fontSize: 60, 
+        textAlign: 'right', 
+        marginBottom:7
+    },
+    resultadoPequeno: {
+        color: 'rgba(255, 255, 255, 0.5)',
+        fontSize: 30, 
+        textAlign: 'right', 
+
+    },
+    fila: {
+        flexDirection: 'row',
+        justifyContent: 'center', 
+        marginBottom: 15, 
+        paddingHorizontal: 10,
+
+    },
+
+    boton: {
+        height: 70, 
+        width: 70, 
+        backgroundColor: '#2D2D2D',
+        borderRadius: 100, 
+        justifyContent: 'center',
+        marginHorizontal:10,
+    }, 
+    botonTexto: {
+        textAlign: 'center',
+        padding: 10, 
+        fontSize: 30, 
+        color: 'white',
+        fontWeight: 'bold', 
+    }
+});
